71b9a9805c66af0ca8a435005cf74507bb2fd2cd
Bryan Lee <hlee0015@student.monash.edu>
2021-10-04 08:40:26 +0800

    Removed sequelize

--- src/server.ts
+++ src/server.ts
@@ -7,19 +7,10 @@ import _ from "lodash";
 import app from "@src/app";
 import ServerConfig from "./config/server.config";
 import { initLogger } from "@src/helper/logger.helper";
-import { sequelize } from "@src/models";
 
 const logger = initLogger("server-start");
 
 // initialise db connection
-sequelize
-  .sync({ alter: true })
-  .then(() => {
-    logger.info("Connection established and synced tables to db");
-  })
-  .catch((err) => {
-    logger.error("Connection error to db", err);
-  });
 
 const serverPort = 16900;
 
